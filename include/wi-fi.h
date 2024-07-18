4c9946ce75188aeb65587640c43cf0e65af027f3
reds6188 <reds6188@gmail.com>
2024-07-18 17:01:13 +0200

    Added "onEvent" and "setCredentials" functions

--- include/wi-fi.h
+++ include/wi-fi.h
@@ -3,13 +3,23 @@
 
 #include <Arduino.h>
 #include <WiFi.h>
+#include <console.h>
+
+#define WIFI_T      "WI-FI"
+
+#define AP_STATIC_IP    { 192, 168, 4, 1 }
+#define AP_GATEWAY_IP   { 192, 168, 4, 0 }
+#define AP_SUBNET_MASK  { 192, 168, 4, 0 }
 
 class WiFiHandler {
 	private:
-		wifi_config_t config;
+		wifi_config_t _sta_config;
+		wifi_config_t _ap_config;
 	public:
-		WiFiHandler(void);
+		WiFiHandler(const char * hostname);
 		void begin(wifi_mode_t mode);
+		void onEvent(void cbOnEvent(WiFiEvent_t event, WiFiEventInfo_t info), WiFiEvent_t event);
+		bool setCredentials(const char* ssid, const char* password);
 };
 
 #endif  /* WI_FI_H_ */
--- src/wi-fi.cpp
+++ src/wi-fi.cpp
@@ -1,7 +1,42 @@
 #include "wi-fi.h"
 
-WiFiHandler::WiFiHandler(void) {
+WiFiHandler::WiFiHandler(const char * hostname) {
+	WiFi.setHostname(hostname);
 }
 
 void WiFiHandler::begin(wifi_mode_t mode) {
+	if(mode == WIFI_AP) {
+	}
+	else if(mode == WIFI_STA) {
+		WiFi.mode(WIFI_STA);
+		WiFi.begin();
+		if(esp_wifi_get_config(WIFI_IF_STA, &_sta_config) != ESP_OK) {
+			console.error(WIFI_T, "Error on getting configuration");
+		}
+		else {
+			console.info(WIFI_T, "SSID = " + String((char *)_sta_config.sta.ssid));
+		}
+	}
+	else if(mode == WIFI_AP_STA) {
+	}
+}
+
+void WiFiHandler::onEvent(void cbOnEvent(WiFiEvent_t event, WiFiEventInfo_t info), WiFiEvent_t event) {
+	WiFi.onEvent(cbOnEvent, event);
+}
+
+bool WiFiHandler::setCredentials(const char* ssid, const char* password)
+{
+	if(esp_wifi_get_config(WIFI_IF_STA, &_sta_config) == ESP_OK) {
+		strncpy((char *)_sta_config.sta.ssid, ssid, 32);
+		strncpy((char *)_sta_config.sta.password, password, 64);
+		if(esp_wifi_set_config(WIFI_IF_STA, &_sta_config) == ESP_OK)
+			return true;
+		else
+			console.error(WIFI_T, "Error on setting configuration");
+	}
+	else
+		console.error(WIFI_T, "Error on getting configuration");
+
+	return false;
 }
